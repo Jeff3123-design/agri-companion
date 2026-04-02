b2a804abc1f3091acecaffd0064d5b632c9623eb
Jeff3123-design <113327553+Jeff3123-design@users.noreply.github.com>
2026-04-02 22:36:53 +0300

    done

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.jefferson.farmbuddyai',
+  appName: 'FarmBuddy-AI',
+  webDir: 'dist'
+};
+
+export default config;
